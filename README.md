f83d5304612560ee4167904084dd37fa5b5550e1
Alessandro Croci <57228872+xela-95@users.noreply.github.com>
2024-01-03 09:13:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+Tutorial on Joint Interface
+===========================
+
+[![Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer)
+
+This tutorial will guide you to the use of **Motor Interfaces** to control the
+iCub movements in the **joint space** relying on the following modalities:
+
+- [**Position Control**](http://www.yarp.it/classyarp_1_1dev_1_1IPositionControl.html)
+- [**Velocity Control**](http://www.yarp.it/classyarp_1_1dev_1_1IVelocityControl.html)
+- [**Position-Direct Control**](http://www.yarp.it/classyarp_1_1dev_1_1IPositionDirect.html)
+
+# Tutorial
+We will show you how to control the elbow joint of the iCub arm in three different
+contexts:
+
+1. With a **single call** delivering the **final position set-point** to the low level layer.
+1. With a **controller thread** that continuously computes the **velocity commands** to reach for a target.
+1. With a **planner thread** streaming suitable **position references** used by the low level layer.
+
+In the end, we will obtain the joint moving as below.
+
+![output](/assets/output.gif)
+
+## How to operate the tutorial
+
+Launch the applications available in [**`./app/scripts`**](./app/scripts) using [**yarpmanager**](http://www.yarp.it/yarpmanager.html): first the "_system_" part and then the "_app_" part.
+
+The three modules open up the following three RPC ports:
+- `/position`
+- `/velocity`
+- `/positiondirect`
+
+Each RPC provides the following services:
+- `go` to initiate the movement.
+- `enc` to read the encoder feedback.
+
+Use case:
+```sh
+$ yarp rpc /position
+>> go
+```
+
+## Notes
+
+You might want to play with our [**yarpmotorgui**](http://www.yarp.it/yarpmotorgui.html), which lets you interact with the joint control of the robot.
+
+To do so, type:
+```sh
+$ yarpmotorgui --robot icubSim
+```
+
+# Diagrams of low-level control modalities
+
+### :large_blue_circle: Position Control
+The high-level user code provides a **one-shot command** in terms of **final position set-point** to achieve along with the **time required** to attain it (or, equivalently, the corresponding speed). Then, the low-level control is responsible for generating a suitable hard-coded trajectory (typically **minimum-jerk** profiles) connecting the current joint angle with the desired set-point as well as for controlling the motor to let the joint reach for the target. Thus, the **control takes place exclusively at the low-level**, whereas the high-level only sends the command to initiate the movement.
+
+![position](/assets/position.png)
+
+### :large_blue_circle: Velocity Control
+The high-level user code is responsible for **continuosly providing joint velocity** to be used to solve a given task defined upstream (e.g. reach a position). The low-level layer is only dealing with the actual control of the motor, **instantly integrating the received velocity** into a position set-point. Therefore, part of the control **complexity is shifted to higher level**.
+
+![velocity](/assets/velocity.png)
+
+### :large_blue_circle: Position Direct Control
+The high-level user code is responsible for **continuosly providing position references** to be used to solve a given task defined upstream. The low-level layer is only dealing with the actual control of the motor. This modality allows for doing **position planning** at higher level, since we may not need to read back the encoders to perform control upstream as in the case of velocity control. As a result, we can achieve **faster movements** (compared with closed-loop approach) with the freedom to **plan generic joint trajectories** (compared with profiles hard-coded in the firmware).
+
+![position-direct](/assets/position-direct.png)
+
+# [How to complete the assignment](https://github.com/vvv-school/vvv-school.github.io/blob/master/instructions/how-to-complete-assignments.md)
---
+++ src/velocity.cpp
@@ -0,0 +1,201 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+//
+// A tutorial on how to use the Gaze Interface.
+//
+// Author: Ugo Pattacini - <ugo.pattacini@iit.it>
+
+#include <cstdlib>
+#include <mutex>
+#include <condition_variable>
+#include <cmath>
+#include <limits>
+
+#include <yarp/os/Network.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/RpcServer.h>
+#include <yarp/os/Time.h>
+
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::dev;
+
+
+class CtrlModule: public RFModule
+{
+protected:
+    PolyDriver        clienJoint;
+    IControlLimits   *ilim;
+    IEncoders        *ienc;
+    IControlMode     *imod;
+    IVelocityControl *ivel;
+
+    RpcServer rpc;
+
+	mutex mtx_done;
+	condition_variable cv_done;
+
+    int joint;
+    double target;
+    bool control;
+
+    void go()
+    {
+        // retrieve joint bounds
+        double min,max,range;
+        ilim->getLimits(joint,&min,&max);
+        range=max-min;
+
+        // retrieve current joint position
+        double enc;
+        ienc->getEncoder(joint,&enc);
+
+        // select target
+        if (fabs(enc-min)<fabs(enc-max))
+            target=max-0.1*range;
+        else
+            target=min+0.1*range;
+
+        // set control mode
+        imod->setControlMode(joint,VOCAB_CM_VELOCITY);
+
+        // remove any possible limitation on the acceleration
+        ivel->setRefAcceleration(joint,numeric_limits<double>::max());
+        
+        // start the control
+        yInfo()<<"Yielding new target: "<<target<<" [deg]";
+        control=true;
+
+        // wait until we're done
+		unique_lock<mutex> lck(mtx_done);
+		cv_done.wait(lck);
+		yInfo()<<"We're done";
+    }
+
+public:
+    virtual bool configure(ResourceFinder &rf)
+    {
+        // open a client interface to connect to the joint controller
+        Property optJoint;
+        optJoint.put("device","remote_controlboard");
+        optJoint.put("remote","/icubSim/left_arm");
+        optJoint.put("local","/velocity/left_arm");
+
+        if (!clienJoint.open(optJoint))
+        {
+            yError()<<"Unable to connect to /icubSim/left_arm";
+            return false;
+        }
+
+        // open views
+        bool ok=true;
+        ok=ok && clienJoint.view(ilim);
+        ok=ok && clienJoint.view(ienc);
+        ok=ok && clienJoint.view(imod);
+        ok=ok && clienJoint.view(ivel);
+
+        if (!ok)
+        {
+            yError()<<"Unable to open views";
+            return false;
+        }
+
+        // elbow
+        joint=3;
+
+        // start with control disabled
+        control=false;
+
+        // target = current joint position
+        ienc->getEncoder(joint,&target);
+
+        // open rpc port
+        rpc.open("/velocity");
+
+        // attach the callback respond()
+        attach(rpc);
+
+        return true;
+    }
+
+    virtual bool close()
+    {
+        rpc.close();
+        clienJoint.close();
+
+        return true;
+    }
+
+    virtual bool respond(const Bottle &cmd, Bottle &reply)
+    {
+        if (cmd.get(0).asString()=="go")
+        {
+            go();
+            reply.addString("ack");
+        }
+        else if (cmd.get(0).asString()=="enc")
+        {
+            double enc;
+            ienc->getEncoder(joint,&enc);
+            reply.addString("ack");
+            reply.addFloat64(enc);
+        }
+        else
+            reply.addString("nack");
+
+        return true;
+    }
+
+    virtual double getPeriod()
+    {
+        // we set up the control rate here in [s]
+        return 0.02;
+    }
+
+    virtual bool updateModule()
+    {
+        if (control)
+        {
+            // retrieve current joint position
+            double enc;
+            ienc->getEncoder(joint,&enc);
+
+            // perform P control
+            double Kp=2.0;
+            double error=target-enc;
+            ivel->velocityMove(joint,Kp*error);
+
+            // notify we're done
+            if (fabs(target-enc)<1.0)
+            {
+                control=false;
+                ivel->velocityMove(joint,0.0);
+				cv_done.notify_all();
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+    if (!yarp.checkNetwork())
+    {
+        yError()<<"YARP doesn't seem to be available";
+        return EXIT_FAILURE;
+    }
+
+    ResourceFinder rf;
+    rf.configure(argc,argv);
+
+    CtrlModule mod;
+    return mod.runModule(rf);
+}
